4da8bad3538db075102c561a792b0603d22bf710
toma3233 <tomabraham6279@gmail.com>
2022-04-29 17:40:37 -0700

    updated python

--- p1_python.py
+++ p1_python.py
@@ -26,22 +26,34 @@ class Token:
         return "Token " + str(self.id) + " = " + self.text + "\nToken type: " + self.token_type + "\n"
         
 
-
-input = "c := 0 % 1 >=  0/ 1;"
-input = input.replace(" ", "")
-id = 0
-i = 0
-# Parsing through input string
-while i < len(input):
-    letter = input[i]
-    # Accounting for operators with two characters
-    if i < len(input) - 1 and input[i+1] == "=" and input [i] in ["=", "<", ">", "!", ":"]:
-        letter = input[i] + input [i+1]
+def parse_and_return(inp):
+    input = inp.replace(" ", "")
+    id = 0
+    i = 0
+    tokens = []
+    # Parsing through input string
+    while i < len(input):
+        letter = input[i]
+        # Accounting for operators with two characters
+        if i < len(input) - 1 and input[i+1] == "=" and input [i] in ["=", "<", ">", "!", ":"]:
+            letter = input[i] + input [i+1]
+            i = i + 1
+        # Create Token obj for each token
+        tok = Token(letter, id)
+        # Append tok obj
+        tokens.append(tok)
+        id += 1
         i = i + 1
-    # Create Token obj for each token
-    tok = Token(letter, id)
-    # Print Token obj 
-    print(tok)
-    id += 1
-    i = i + 1
+
+    return tokens
+
+if __name__ == "__main__":
+    input = "b:= 1*1;"
+    list_of_tokens = parse_and_return(input)
+    for tok in list_of_tokens:
+        print(tok)
+
+
+
+
 
